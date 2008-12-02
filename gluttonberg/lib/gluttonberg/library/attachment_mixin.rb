db7aa854af8efd983f9b10e8d61e1df17e486590
Anthony Richardson <anthony@anthonyrichardson.com>
2008-12-02 17:21:17 +1030

    basic support for thumb-nailing images

--- gluttonberg/lib/gluttonberg/library/attachment_mixin.rb
+++ gluttonberg/lib/gluttonberg/library/attachment_mixin.rb
@@ -1,3 +1,5 @@
+require 'image_science'
+
 module Gluttonberg
   module Library
     module AttachmentMixin
@@ -18,6 +20,13 @@ module Gluttonberg
         unless new_file.blank?
           # Forgive me this naive sanitisation, I'm still a regex n00b
           clean_filename = new_file[:filename].gsub(" ", "_").gsub(/[^A-Za-z0-9\-_.]/, "").downcase
+          
+          # _thumb.jpg is a reserved name for the thumbnailing system, so if the user
+          # has a file with that name rename it.
+          if (clean_filename == '_thumb_small.jpg') || (clean_filename == '_thumb_large.jpg')
+            clean_filename = 'thumb.jpg'
+          end
+          
           attribute_set(:file_name, clean_filename)
           attribute_set(:size, new_file[:size])
           @file = new_file
@@ -31,6 +40,14 @@ module Gluttonberg
       def url
         "/assets/#{hash}/#{file_name}"
       end
+        
+      def thumb_small_url
+        "/assets/#{hash}/_thumb_small.jpg"
+      end
+
+      def thumb_large_url
+        "/assets/#{hash}/_thumb_large.jpg"
+      end
       
       def location_on_disk
         directory / file_name
@@ -38,6 +55,36 @@ module Gluttonberg
       
       def directory
         Library.root / hash
+      end
+
+      def generate_thumb
+        ImageScience.with_image(location_on_disk) do |img|
+#          img.thumbnail(64) do |thumb|
+#            thumb.save File.join(directory,'_thumb.jpg')
+#          end
+
+         # small = 110w x 75h
+         # large = 250w x 200h
+
+         if (img.height >= img.width) then
+            # scale to height
+            img.thumbnail(75) do |thumb|
+              thumb.save File.join(directory,'_thumb_small.jpg')
+            end
+            img.thumbnail(200) do |thumb|
+              thumb.save File.join(directory,'_thumb_large.jpg')
+            end
+          else
+            # scale to width
+            img.thumbnail(110) do |thumb|
+              thumb.save File.join(directory,'_thumb_small.jpg')
+            end
+            img.thumbnail(250) do |thumb|
+              thumb.save File.join(directory,'_thumb_large.jpg')
+            end
+          end
+
+        end
       end
 
       private
@@ -52,6 +99,9 @@ module Gluttonberg
         if file
           FileUtils.mkdir(directory) unless File.exists?(directory)
           FileUtils.cp file[:tempfile].path, location_on_disk
+
+          # new file has been upload, if it is an image, then create a thumbnail
+          generate_thumb
         end
       end
       
