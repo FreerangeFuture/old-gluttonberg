40ae0542be81c17427eb1e6035a563b568d80a23
Anthony Richardson <anthony@anthonyrichardson.com>
2009-01-16 01:00:10 +1030

    fleshed out gluttonberg partials more

--- gluttonberg/lib/gluttonberg/red_cloth_partials.rb
+++ gluttonberg/lib/gluttonberg/red_cloth_partials.rb
@@ -1,19 +1,30 @@
+#
+# Red Cloth Partials
+#
+# This module adds support to access Merb Parts from within Gluttonberg
+# RichTextContent Sections.
+#
+# To enable a merb Part to be available to Gluttonberg you need to use the
+# partials class method. See the example below.
+#
+#
+
 require 'merb-parts'
 
 module Gluttonberg
   module RedClothPartials
     module PartsControllerMixin
-      @@partials_class_list = []
+      @@partials_definition_list = {}
 
       def self.included(klass)
         klass.class_eval do
 
-          # register this PartsContollers actions with a specific route. This is aroute
+          # register this PartsContollers actions with a specific route. This is a route
           # used in the RedCloth extension to embed partials data into a
           # page section using RedCloth
           #
           #   class Films < PartController
-          #     partials :summary, {:recent => :latest}
+          #     partials :summary, :recent
           #     partial_route :movies
           #
           #     def summary(id)
@@ -32,55 +43,62 @@ module Gluttonberg
           #   irb> RedCloth.new('blah blah {{movies/summary/4}} blah blah').to_html
           #   > "<p>blah blah <div class='film'>4</div> blah blah</p>"
           #
-          #   irb> RedCloth.new('blah blah {{movies/latest}} blah blah').to_html
+          #   irb> RedCloth.new('blah blah {{movies/recent}} blah blah').to_html
           #   > "<p>blah blah <div class='film'>78</div> blah blah</p>"
           #
           def self.partials(*actions)
+
+            routedef = @@partials_definition_list[self]
+            routedef = RouteDefinition.new(self) unless routedef
+
             actions.each do |action|
-
-
-              routedef = RouteDefination.new(self, action, route)
-              @@partials_class_list << routedef
+              routedef.add_action(action)
             end
           end
 
-          def self.partial_route(route)
-            
+          # see example for partials()
+          def self.partial_route(route_name)
+            routedef = @@partials_definition_list[self]
+            routedef = RouteDefinition.new(self) unless routedef
+
+            routedef.controller_route_name = route_name
           end
         end
       end
 
     end
 
-    class RouteDefination
+    class RouteDefinition
       attr_accessor :controller_route_name
       attr_accessor :controller
       attr_accessor :actions
 
-      def initialization(*action_definations)
+      def initialization(cont)
         @actions = []
+        self.controller = cont
+        self.controller_route_name = cont.to_s
+      end
 
-        *action_definations.each do |action_def|
-          if action_def.is_a?(Hash) then
-            
-          else
-            action_def = action_def.to_sym
-
-
-          end
-        end
+      def add_action(action)
+        @actions << action
       end
     end
 
     class Renderer
+
+      # Called within a view or controller action to process
+      # the string data in "content". If "content" contains
+      # partial calls they are processed and preplaced with
+      # the results of those partials.
+      #
       def self.render(controller, content)
-        content.scan(/(\{\{(\w+\/\w+[\w|\/]*)\}\})/) do |match|
-          if match.length == 2 then
-            partial_url = match[1]
-            p partial_url
-            return controller.part(Bar => :baz)
-          end
-        end
+
+        #TODO: Parse content and replace occurancesof {{x/y/z}} with the
+        #      merb parts result within the {{ }}
+        #      lookup the correct part to use by looking in the
+        #      PartsControllerMixin.@@partials_definition_list for the
+        #      matching entry
+
 
         content
       end
