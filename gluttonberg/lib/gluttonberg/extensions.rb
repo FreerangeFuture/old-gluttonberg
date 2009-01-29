a91fbc6c652a7ef3bbbb7280988e4e1100335200
Luke Sutton <luke@freerangefuture.com>
2009-01-29 16:49:06 +1030

    Tidy up the lib dir a little bit by moving extensions into their own dir.

---
+++ gluttonberg/lib/gluttonberg/extensions.rb
@@ -0,0 +1,5 @@
+extensions = Pathname(__FILE__).dirname.expand_path
+
+require extensions / "extensions" / "array"
+require extensions / "extensions" / "datamapper"
+require extensions / "extensions" / "merb_helpers"
