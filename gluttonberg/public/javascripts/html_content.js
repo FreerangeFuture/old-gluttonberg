311a0d3c46652e299aa024e689d4db385e8b0adf
Taryn Hicks <taryn@Tarale.local>
2009-07-29 13:18:33 +0930

    Changed TinyMCE controls to remove undesirable formatting options, include remove formatting button

--- gluttonberg/public/javascripts/html_content.js
+++ gluttonberg/public/javascripts/html_content.js
@@ -8,8 +8,8 @@ tinyMCE.init({
                 plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
                 // Theme options
-                theme_advanced_buttons1 : "fullscreen,|,cleanup,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,|,bullist,numlist,|,blockquote,|,sub,sup,",
-                theme_advanced_buttons2 : "search,replace,|,undo,redo,|,link,unlink,anchor,|,tablecontrols",
+                theme_advanced_buttons1 : "fullscreen,|,removeformat,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,styleselect,|,bullist,numlist,|,blockquote,|,sub,sup,",
+                theme_advanced_buttons2 : "undo,redo,|,link,unlink,anchor,|,tablecontrols",
                 theme_advanced_buttons3 : "",
                 theme_advanced_toolbar_location : "top",
                 theme_advanced_toolbar_align : "left",
